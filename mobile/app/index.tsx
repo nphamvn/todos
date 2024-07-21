8bde7bbb9f15c26f6aca159ccb5324158b67a42a
Nam Pham <pvnam95@hotmail.com>
2024-07-21 23:01:30 +0900

    refactoring

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,5 @@
 import { Link, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -6,18 +7,31 @@ import {
   SafeAreaView,
   TouchableHighlight,
 } from "react-native";
-
+interface List {
+  id: string;
+  name: string;
+}
 export default function Screen() {
   const router = useRouter();
+  const [lists, setLists] = useState<List[]>([]);
+
+  useEffect(() => {
+    setLists(
+      Array.from({ length: 10 }).map((_, i) => ({
+        id: i.toString(),
+        name: `List ${i}`,
+      }))
+    );
+  }, []);
+
   return (
     <SafeAreaView
       style={{
         backgroundColor: "white",
       }}
     >
-      <Link href={"debug"}>debug</Link>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        {Array.from({ length: 50 }).map((_, i) => (
+        {lists.map((_, i) => (
           <TouchableHighlight
             key={i}
             underlayColor={"#f0f0f0"}
